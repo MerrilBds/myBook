509893cd5f130ef1316999ba33a0defbc6a2eca9
MerrilBds <Bdsmerril@gmail.com>
2023-02-27 22:11:14 +0100

    novel1 fin

---
+++ audio.js
@@ -0,0 +1,29 @@
+var audio = document.getElementById('audio');
+var playPauseBTN = document.getElementById('playPauseBTN');
+var count = 0;
+
+function playPause(){
+	if(count == 0){
+		count = 1;
+		audio.play();
+		playPauseBTN.innerHTML = "Pause ⏸";
+	}else{
+		count = 0;
+		audio.pause();
+		playPauseBTN.innerHTML = "Play ►";
+	}
+
+}
+
+function stop(){
+	playPause()
+	audio.pause();
+	audio.currentTime = 0;
+	playPauseBTN.innerHTML = "Play ►";
+}
+$(document).ready(function(){
+    $("body").ripples({
+    resolution: 200,
+    perturbance: .004,
+});
+});
--- main.js
+++ main.js
@@ -81,8 +81,9 @@ function goPrevPage() {
         currentLocation--;
     }
 }
+
 $(document).ready(function(){
-    $(".effect").ripples({
+    $("body").ripples({
     resolution: 200,
     perturbance: .004,
 });
